387c6eec3d29d58a0e040899b31b18311f649305
Paul Khomich <paulkhomich@outlook.com>
2020-02-06 19:14:50 +0300

    auto

--- collatz.sh
+++ collatz.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 n=${1:-0}
-c=0
 
+if [[ n -lt 1 ]]; then
+    echo "usage: ${0:2} [num > 0]"
+    exit 1
+fi
+
+c=0
 while [[ n -ne 1 ]]; do
     if [[ $(( n  % 2 )) -eq 0 ]]; then
 	n=$(( n / 2 ))
